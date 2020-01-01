b001de0761c8c00e1052be07295eafcd9a65472a
phantasmicmeans <dlaak0630@naver.com>
2020-01-02 02:29:23 +0900

    [FCM] init 로직 추가

--- init.go
+++ init.go
@@ -3,10 +3,40 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
 	"google.golang.org/api/option"
 )
+
+var firebaseApp *firebase.App
+var fcmCtx = context.Background()
+var fcmClient *messaging.Client
+
+//init function
+func init() {
+	//[INIT START]
+	log.Println("[INIT START]")
+	getFirebaseInstance()
+	log.Printf("[FIREBASE.APP INIT COMPLETED]")
+	getFcmClient()
+	log.Printf("[FCM CLIENT INIT COMPLETED]")
+}
+
+func getFirebaseInstance() *firebase.App {
+	if firebaseApp == nil {
+		firebaseApp = initializeAppWithServiceAccount()
+	}
+	return firebaseApp
+}
+
+func getFcmClient() *messaging.Client {
+	if fcmClient == nil {
+		fcmClient = initClient(getFirebaseInstance())
+	}
+	return fcmClient
+}
 
 func initializeAppWithServiceAccount() *firebase.App {
 	opt := option.WithCredentialsFile(serviceKey)
@@ -16,4 +46,13 @@ func initializeAppWithServiceAccount() *firebase.App {
 	}
 	// [END intialize_app_service_account_golang]
 	return app
+}
+
+func initClient(app *firebase.App) *messaging.Client {
+	client, err := app.Messaging(fcmCtx)
+	if err != nil {
+		log.Fatalf("ERROR GETTING MESSAGING CLIENT: %v\n", err)
+		os.Exit(1)
+	}
+	return client
 }
