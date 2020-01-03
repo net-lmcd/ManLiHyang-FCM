24e5178964e8a5315e6d3cf8019a1b8e6926c0af
phantasmicmeans <phantasmicmeans@kakao.com>
2020-01-03 16:55:19 +0900

    error message

--- const.go
+++ const.go
@@ -1,4 +1,5 @@
 package main
 
 const testToken = "dUXt1GJqxqg:APA91bFccWDj8LxaG0J6UjsZJilxEvjv7TdvGtJTUZyCKNpK3KRixF0-pwDI-3vUp1oUFbm9ex3u7H0MUlcC44D3_hU6DCNDkGGBn40Dixd0hFy2m6bvm-7g0VCIq2n09_QXdpWxCy6Z"
-const serviceKey = "/Users/phantasmicmeans/firebase/manlihyang-55bb9-firebase-adminsdk-jicmz-02b377bcfc.json"
+const serviceKey = "/Users/user/firebase/manlihyang-55bb9-firebase-adminsdk-jicmz-02b377bcfc.json"
+const serverErrorMessage = "Internal Server Error"
